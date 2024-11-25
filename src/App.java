6e8a9dfa4096230c60a2e3cf1602d111437655b7
CodyMarkix <marekplasek08@protonmail.com>
2024-11-25 17:39:38 +0100

    Houston, mission success

--- src/App.java
+++ src/App.java
@@ -1,21 +1,36 @@
 import cz.gyarab.util.light.Matrix;
-import java.util.Base64;
 
 class CubeFace {
     private int dotCount;
     public Matrix matrix = Matrix.createMatrix(3, 3);
-    String numData = "1-1,1;2-2,0:0,2;3-0,2:1,1:2,0;4-0,0:0,2:2,0:2,2;5-0,0:0,2:1,1:0,2:2,0;6-0,0:0,1:0,0:2,0:2,1:2,2"; // count1-x1,y1;count2-x1,y1,x2,y2; and on and on...
+    
+    // Dle mého názoru je tohle lepší než hardcodenout masivní switch statement
+    String numData = "1-1,1;2-2,0:0,2;3-0,2:1,1:2,0;4-0,0:0,2:2,0:2,2;5-0,0:2,0:1,1:0,2:2,2;6-0,0:0,1:0,2:2,0:2,1:2,2";
+    /*
+     * Každý set souřadnic pro jedno číslo je rozdělen středníkem.
+     * V samotném setu je potom číslo, které náelží tomuto setu a samotný set dat, rozděleny pomlčkou.
+     * Každá souřadnice je potom rozdělena dvojtečkou
+     * 
+     * PŘÍKLAD JEDNOHO SETU:
+     * 4-
+     *  0,0:
+     *  0,2:
+     *  2,0:
+     *  2,2
+     */
     
     public CubeFace(int number) {
         this.dotCount = number;
     }
 
-    public int getDotCount() {
+    public int getNumber() {
         return this.dotCount;
     }
 
-    public void setDotCount(int newDotCount) {
+    public void setNumber(int newDotCount) throws Exception {
+        this.clearMatrix();
         this.dotCount = newDotCount;
+        this.drawMatrix();
     }
 
     String[] getCoordData(int number) {
@@ -31,23 +46,46 @@ class CubeFace {
             }
         }
 
-        return new String[0];
+        return new String[0]; // Sem se dostanem jen pokud nenajdem žádné data pro hledané číslo
+    }
+    
+    void showWindow() {
+        this.matrix.showWindow();
     }
 
-    void DrawMatrix() {
+    void clearMatrix() {
+        for (int y = 0; y < this.matrix.getHeight(); y++) {
+            for (int x = 0; x < this.matrix.getWidth(); x++) {
+                this.matrix.setOff(x, y);
+            }
+        }
+    }
+
+    void drawMatrix() throws Exception {
         String coordData[] = this.getCoordData(this.dotCount);
-        this.matrix.showWindow();
 
         for (int i = 0; i < coordData.length; i++) {
-            System.out.println(coordData[i]);
+            int x = Integer.parseInt(coordData[i].split(",")[0]);
+            int y = Integer.parseInt(coordData[i].split(",")[1]);
+
+            System.out.print(x);
+            System.out.print(" ");
+            System.out.println(y);
+
+            this.matrix.setOn(x, y);
         }
     }
 }
 
-
 public class App {
     public static void main(String[] args) throws Exception {
-        CubeFace cb = new CubeFace(4);
-        cb.DrawMatrix();
+        CubeFace cb = new CubeFace(1);
+        cb.showWindow();
+
+        for (int i = 1; i <= 6; i++) {
+            cb.setNumber(i);
+            cb.drawMatrix();
+            Thread.sleep(2000);
+        }
     }
 }
